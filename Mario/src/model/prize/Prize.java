5f27fef41a532fcd840281f124c92f761e436e48
SamuelSara <SamScottSarantos@calbaptist.edu>
2024-03-02 12:56:31 -0800

    Initial commit

---
+++ Mario/src/model/prize/Prize.java
@@ -0,0 +1,18 @@
+package model.prize;
+
+import Functions.GameEngine;
+import model.hero.Mario;
+
+import java.awt.*;
+
+public interface Prize {
+
+    int getPoint();
+
+    void reveal();
+
+    Rectangle getBounds();
+
+    void onTouch(Mario mario, GameEngine engine);
+
+}
